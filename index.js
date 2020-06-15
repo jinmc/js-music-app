5b2b946366baf825795d60ed0fc5e6a1e639d4f0
Jim Chong <jinmo55555@gmail.com>
2020-06-15 12:18:22 -0400

    reset while playing

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound')
     const pads = document.querySelectorAll(".pads div")
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#5e2aa1",
+        "#2aabcb",
+    ];
 
     console.log(sounds)
 
     pads.forEach((pad, index) => {
         pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
             sounds[index].play();
         });
     });
